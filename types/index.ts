addd85d9881f73fbebaa3214c99a59c47200df02
void <95859137+voiiddxx@users.noreply.github.com>
2024-05-07 23:45:00 +0530

    Added some condition

--- types/index.ts
+++ types/index.ts
@@ -2,9 +2,21 @@
 
 
 export type CreateUserParams = {
-    data:{
-        username:string
-        email:string
-        avatar:string
+    data: {
+        username: string
+        email: string
+        avatar: string
+    }
+}
+
+
+export type UpdateUserParams = {
+    userId:number
+    data: {
+        loses?: number,
+        avatar?: string,
+        wonmatch?: number,
+        role?: number,
+        matchPlayed?: number
     }
 }
