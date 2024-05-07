01b36397f27b986c1fb2466c718feff062c05672
void <95859137+voiiddxx@users.noreply.github.com>
2024-05-07 23:36:02 +0530

    added action for creating user account

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,7 +1,7 @@
 "use server"
 
 import { CreateUserParams } from "@/types";
-import { PrismaClient } from "@prisma/client/extension";
+import { PrismaClient } from '@prisma/client'
 
 
 
@@ -14,7 +14,22 @@ export const CreateUserAction = async({data}: CreateUserParams)=>{
         if(!data){
             return JSON.parse(JSON.stringify({message:"No Credentials found" , status : 400}));
         }
-        
+        const userRes = await prisma.user.create({
+            data:{
+                username:'nbjb',
+                avatar:'nn ',
+                email:'mlm',
+                loses:0,
+                matchplayed:0,
+                role:'Player',
+                tied:0,
+                wonmatch:0,
+            }
+        });
+        if(!userRes){
+            return JSON.parse(JSON.stringify({message:"Some error occured" , status:400}));
+        }
+        return JSON.parse(JSON.stringify({data:userRes , status:200}));
     } catch (error) {
         console.log(error);
         
@@ -36,3 +51,6 @@ export const UpdateProfileAction = async()=>{
         
     }
 }
+
+
+
