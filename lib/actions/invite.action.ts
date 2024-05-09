addff7d47aad3f1f34e0603b998b7f1debe2b6dd
void <95859137+voiiddxx@users.noreply.github.com>
2024-05-09 23:32:36 +0530

    added some bilerplate

--- lib/actions/invite.action.ts
+++ lib/actions/invite.action.ts
@@ -1,1 +1,19 @@
 "use server"
+
+import { PrismaClient } from "@prisma/client"
+
+
+// prisma config
+
+const prisma = new PrismaClient();
+
+
+// server action for creating the match invitation
+export const CreateMatchInvitationAction = async ()=>{
+    try {
+        
+    } catch (error) {
+        console.log(error);
+        
+    }
+}
