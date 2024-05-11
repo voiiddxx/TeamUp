e992730cf899ce4c7ec4a48b6d572ebd39d7a4eb
void <95859137+voiiddxx@users.noreply.github.com>
2024-05-11 12:03:08 +0530

    some changes in middlewares

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,15 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { authMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware(
-    
-);
+export default authMiddleware({
+    publicRoutes:[
+        '/',
+        '/api/webhook/clerk'
+    ],
+    ignoredRoutes:[
+        '/',
+        '/api/webhook/clerk'
+    ]
+});
 
 export const config = {
   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
