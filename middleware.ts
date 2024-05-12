cd010cba38419408b9f73912cae72d07670faef0
void <95859137+voiiddxx@users.noreply.github.com>
2024-05-13 00:42:28 +0530

    added changes inm models

--- middleware.ts
+++ middleware.ts
@@ -1,17 +1,41 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default authMiddleware({
-    publicRoutes:[
-        '/',
-        '/api/webhook'
-    ],
-    ignoredRoutes:[
-        '/',
-        '/api/webhook'
-    ]
+
+// export const authMiddleware();
+const isProtectedRoute = createRouteMatcher([
+    '/',
+    '/dashboard',
+])
+export default clerkMiddleware((auth , req)=>{
+    if(isProtectedRoute(req)) auth().protect();
 });
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-  
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
 };
+
+
+
+
+
+
+
+
+
+// import { authMiddleware } from "@clerk/nextjs/server";
+
+// export const authMiddleware({
+//     publicRoutes:[
+//         '/',
+//         '/api/webhook'
+//     ],
+//     ignoredRoutes:[
+//         '/',
+//         '/api/webhook'
+//     ]
+// });
+
+// export const config = {
+//   matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+  
+// };
