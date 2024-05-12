cfa43d2e9ffe6b262cd87626ac40bb1e1f4a9287
void <95859137+voiiddxx@users.noreply.github.com>
2024-05-12 11:32:08 +0530

    added action for getting all the matches

--- lib/actions/match.action.ts
+++ lib/actions/match.action.ts
@@ -14,36 +14,71 @@ const prisma = new PrismaClient();
 
 // server action for creating the match
 
-export const CreatMatchAction = async ({data}:CreateMacthParams)=>{
-    if(!data){
-        return JSON.parse(JSON.stringify({message:"No Data Found" , status:400}));
+export const CreatMatchAction = async ({ data }: CreateMacthParams) => {
+    if (!data) {
+        return JSON.parse(JSON.stringify({ message: "No Data Found", status: 400 }));
     }
-    const matchRes = await  prisma.match.create({
-        data:{
-            bet:2,
-            location:data.location,
-            status:'11',
-            time:data.date,
-            matchCategoryId:5151521,
-            createdteamId:2211,
-            joinedteamid:3211,
-            looserteamid:2111,
-            winningteamid:11515,
+    const matchRes = await prisma.match.create({
+        data: {
+            bet: 2,
+            location: data.location,
+            status: '11',
+            time: data.date,
+            matchCategoryId: 5151521,
+            createdteamId: 2211,
+            joinedteamid: 3211,
+            looserteamid: 2111,
+            winningteamid: 11515,
         }
     });
-    if(!matchRes) {
-        return JSON.parse(JSON.stringify({message:"Error while creating the match" , status:401}));
+    if (!matchRes) {
+        return JSON.parse(JSON.stringify({ message: "Error while creating the match", status: 401 }));
     }
-    return JSON.parse(JSON.stringify({data:matchRes , status:200}));
+    return JSON.parse(JSON.stringify({ data: matchRes, status: 200 }));
 }
 
 
 
 // server action for gettig the match based on id
 
-export const GetMatchWithIdAction  = async (matchId : number)=>{
-    if(!matchId){
-        return JSON.parse(JSON.stringify({message:"No Data found" , staus:400}));
+export const GetMatchWithIdAction = async (matchId: number) => {
+    if (!matchId) {
+        return JSON.parse(JSON.stringify({ message: "No Data found", staus: 400 }));
     }
-    
+    const matchRes = await prisma.match.findFirst({
+        where: {
+            matchid: matchId
+        },
+        include: {
+            Category: true,
+            createdTeam: true,
+            invites: true,
+            Joinedteam: true,
+            looserteam: true,
+            winningteam: true,
+            
+        }
+    });
+
+    if (!matchRes) {
+        return JSON.parse(JSON.stringify({ message: "Some error while getting the team data", status: 401 }));
+    }
+    return JSON.parse(JSON.stringify({ data: matchRes, status: 200 }));
+}
+
+
+// server action for getting the info all all the matches out thete
+
+export const GetAllMatchesAction = async()=>{
+    try {
+        const res = await prisma.match.findMany({});
+        if(!res){
+            return JSON.parse(JSON.stringify({message:"No Matches Found" , status:400}));
+        }
+        return JSON.parse(JSON.stringify({data:res , status:200}));
+
+    } catch (error) {
+        console.log(error);
+        
+    }
 }
