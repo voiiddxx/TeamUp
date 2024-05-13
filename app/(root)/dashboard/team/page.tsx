a1813601c0d32d823a7fa6cb5ac903c33a7a1e2c
void <95859137+voiiddxx@users.noreply.github.com>
2024-05-14 01:22:39 +0530

    Added new component for getting my matches

---
+++ app/(root)/dashboard/team/page.tsx
@@ -0,0 +1,13 @@
+import MyTeam from '@/components/shared/team/MyTeam'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+        this is the page for my Team
+        <MyTeam/>
+    </div>
+  )
+}
+
+export default page
--- components/shared/team/MyTeam.tsx
+++ components/shared/team/MyTeam.tsx
@@ -1,11 +1,18 @@
 "use client"
+import { GetUserTeamAction } from '@/lib/actions/team.action'
 import React, { useEffect } from 'react'
 
 
 const MyTeam = () => {
 
     useEffect(()=>{
-        
+        const GetMyTeam = async ()=>{
+          const user = localStorage.getItem("x-auth-user");
+          const res = await GetUserTeamAction(+user!);
+          console.log("This is value of res: " , res);
+          
+        }
+        GetMyTeam();
     })
   return (
     <div className='flex justify-center items-center flex-col' >
