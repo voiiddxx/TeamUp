33c5484da4f833eecfabc0740d2adca3102787db
void <95859137+voiiddxx@users.noreply.github.com>
2024-05-13 23:11:27 +0530

    sported the issue getting from resend

--- emails/index.tsx
+++ emails/index.tsx
@@ -1,11 +1,9 @@
 // import { Button, Html } from "@react-email/components";
-import { Html } from "next/document";
+// import { Html } from "next/document";
 import * as React from "react";
 
 export default function Email() {
   return (
-    <Html>
-        <h1>hey this is void</h1>
-    </Html>
+    <p>this is good</p>
   );
 }
