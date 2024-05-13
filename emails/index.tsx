9f611bb436a28f7b11b340f80e2cc518f8cf366b
void <95859137+voiiddxx@users.noreply.github.com>
2024-05-13 23:06:23 +0530

    sorting out the issue of punnycode

---
+++ emails/index.tsx
@@ -0,0 +1,11 @@
+// import { Button, Html } from "@react-email/components";
+import { Html } from "next/document";
+import * as React from "react";
+
+export default function Email() {
+  return (
+    <Html>
+        <h1>hey this is void</h1>
+    </Html>
+  );
+}
