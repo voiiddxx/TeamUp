08bf0b76fbfb13eeeef708b9da8f05f9f93b62a2
void <95859137+voiiddxx@users.noreply.github.com>
2024-05-17 22:00:15 +0530

    Add: Main Nav

---
+++ app/(root)/explore/page.tsx
@@ -0,0 +1,13 @@
+import MainNavBar from '@/components/shared/navbars/MainNavBar'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='min-h-screen w-full bg-zinc-900' >
+        <MainNavBar/>
+        this is main page.tsx file
+    </div>
+  )
+}
+
+export default page
